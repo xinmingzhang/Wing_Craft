6a010ed77393591ab7b87fa0e738bedb0ff258c9
xmzhang <252004372@qq.com>
2017-02-24 22:26:20 +0800

    smile

--- run_game.py
+++ run_game.py
@@ -1,10 +1,10 @@
 if __name__ == '__main__':
-    # try:
-    #     from data.smile import Smile
-    #     smile = Smile()
-    #     smile.run()
-    # except:
-    #     print('Sorry, something wrong.')
+    try:
+        from data.smile import Smile
+        smile = Smile()
+        smile.run()
+    except:
+        print('Sorry, something wrong.')
     import sys
     import pygame as pg
     from data.main import main
